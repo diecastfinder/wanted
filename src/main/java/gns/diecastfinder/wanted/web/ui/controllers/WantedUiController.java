0a78470f63552a2f96385a15d5840e6f339227b7
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-05-26 21:17:32 +0200

    Issue#4 - http://localhost:8082/wanted-models displays all models in DB.

---
+++ src/main/java/gns/diecastfinder/wanted/web/ui/controllers/WantedUiController.java
@@ -0,0 +1,23 @@
+package gns.diecastfinder.wanted.web.ui.controllers;
+
+import gns.diecastfinder.wanted.repositories.WantedModelRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WantedUiController {
+
+    private final WantedModelRepository wantedModelRepository;
+
+    public WantedUiController(WantedModelRepository wantedModelRepository) {
+        this.wantedModelRepository = wantedModelRepository;
+    }
+
+    @RequestMapping("/wanted-models")
+    public String getWantedModels(Model model) {
+        model.addAttribute("wantedModels", wantedModelRepository.findAll());
+        return "wantedmodels/list";
+    }
+
+}
