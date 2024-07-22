a42c9dc437f42365f885e0d31bad11d52247e9e2
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-07-22 20:18:16 +0200

    add JSON logging configuration with logback

--- src/main/java/gns/diecastfinder/wanted/WantedApplication.java
+++ src/main/java/gns/diecastfinder/wanted/WantedApplication.java
@@ -2,7 +2,9 @@ package gns.diecastfinder.wanted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class WantedApplication {
 	public static void main(String[] args) {
